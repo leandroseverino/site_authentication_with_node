14c64bb7a5822d4b64e9fda1cb171386e6846265
unknown <Leandro Severino@POAW10N09344.resource.com.br>
2018-12-26 20:08:17 -0200

    Add mongoose support

---
+++ models/user.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    username: String,
+    email: String,
+    password: String
+}, {
+    timestamps: {
+        createdAt: 'createdAt',
+        updatedAt: 'updatedAt'
+    }
+});
+
+const User = mongoose.model('user', userSchema);
+
+module.exports = User;
--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const Joi = require('joi');
 
+const User = require('../models/user');
+
 const userSchema = Joi.object().keys({
   email: Joi.string().email().required(),
   username: Joi.string().required(),
@@ -13,12 +15,26 @@ router.route('/register')
   .get((req, res) => {
     res.render('register');
   })
-  .post((req, res, next) => {
-    const result = Joi.validate(req.body, userSchema);
-    if (result.error) {
-      req.flash('error', 'Data is not valid !. Please try again. !');
-      res.redirect('/users/register');
+  .post(async (req, res, next) => {
+    try {
+      const result = Joi.validate(req.body, userSchema);
+      if (result.error) {
+        req.flash('error', 'Data is not valid !. Please try again. !');
+        res.redirect('/users/register');
+        return;
+      }
+      const user = await User.findOne({ 'email': result.value.email });  
+      if (user) {
+        req.flash('error', 'Email alredy in use !.');
+        res.redirect('/users/register');
+        return;
+      } else {
+        
+      }
+    } catch (error) {
+      next(error);
     }
+    
   });
 
 router.route('/login')
